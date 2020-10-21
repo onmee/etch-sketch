9e805d0d4b9b1d222811537a2c82650b92189c67
SubashA <subashadhikari@outlook.com>
2020-10-21 16:39:15 +0100

    Create all feature functions

--- main.js
+++ main.js
@@ -1,33 +1,66 @@
-//Create a 64x64 = 4096 divs and append to container div
-//Add class container-item to each div
+// Declare n but leave it undefined
+let n = 32;
 
-for (i=0; i < 4096; i++) {
-    let container = document.querySelector(".container")
-    let div = document.createElement("div")
-    div.classList.add('container-item')
-    container.appendChild(div)
+// Set the background of container items to whiteS
+
+function resetColor() {
+  const cells = document.querySelectorAll('.container-item');
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].style.background = 'white';
+  }
 }
 
+// An elements background style is changed when the mouse is clicked and moved.
+// Changes are effected by setting isClicked to True.
 
-// The elements background is changed only 
-// when the mouse is clicked. held and moved.
-// Changes effected by setting isClicked to True of False. 
+function draw() {
+  let isClicked = false;
+  const cells = document.querySelectorAll('.container-item');
 
-let isClicked = false;
-const cells = document.querySelectorAll('.container-item');
-
-cells.forEach(item => item.addEventListener('mousedown', event => {
+  cells.forEach((item) => item.addEventListener('mousedown', () => {
     isClicked = true;
-    
-}))
+  }));
 
-cells.forEach(item => item.addEventListener('mousemove', event => {
+  cells.forEach((item) => item.addEventListener('mousemove', (event) => {
     if (isClicked === true) {
-        event.target.style.background = 'black'
+      event.target.style.background = 'black';
     }
-}))
+  }));
 
-cells.forEach(item => item.addEventListener('mouseup', event => {
+  cells.forEach((item) => item.addEventListener('mouseup', () => {
     isClicked = false;
-    
-}))
+  }));
+}
+
+// Remove previous child nodes from container
+
+function removeChildNodes() {
+  const node = document.getElementById('container');
+  while (node.firstChild) {
+    node.removeChild(node.lastChild);
+  }
+}
+
+function gridSize() {
+  resetColor();
+  removeChildNodes();
+
+  // Prompt user to input size of grid, between 16 and 100.
+  // Ensure only a number is provided.
+  do {
+    n = parseInt(prompt('Enter number between 16 and 100:'), 10);
+  } while (isNaN(n) || n > 100 || n < 16);
+
+  // Creat and add the required number of divs to the container.
+  for (let i = 0; i < n * n; i++) {
+    const container = document.getElementById('container');
+    container.style.gridTemplateColumns = `repeat(${n},1fr)`;
+    const div = document.createElement('div');
+    div.classList.add('container-item');
+    container.appendChild(div);
+  }
+  draw();
+}
+
+// When START button is clicked, run the gridSize function.
+document.getElementById('start').onclick = gridSize;
